369255ba6ab9dabe63d804063ce70db55f5a5498
fexfoundryalloys <enquiry@fexalloys.com>
2026-06-14 20:56:57 +0530

    Revert "header footer text sample 1"
    This reverts commit 17712282a5f405d2b3da4569200963ae6895371f.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -88,9 +88,11 @@ export default function Navbar({ currentTab, setTab, onSelectProduct }: NavbarPr
             className="cursor-pointer hover:opacity-80 transition-opacity shrink-0"
             id="nav-logo"
           >
-            <span className="font-display font-black text-lg tracking-wide text-dark-navy uppercase select-none">
-              Fe<span className="text-industrial-red">X</span> FOUNDRY ALLOYS
-            </span>
+            <img
+              src={`${import.meta.env.BASE_URL}logo/logofexalloys.png`}
+              alt="FeX Foundry Alloys"
+              className="h-8 w-auto object-contain"
+            />
           </div>
 
           {/* Desktop Nav Links */}
